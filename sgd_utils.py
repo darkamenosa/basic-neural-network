d722f1ebf6ceb6ecce1f92cf8eb0f784536a23ce
tuyenhx <hxtxmu@gmail.com>
2017-09-07 00:34:58 +0700

    fix bug initialize parameters

--- sgd_utils.py
+++ sgd_utils.py
@@ -77,7 +77,7 @@ def initialize_parameters(layer_dims):
         # W3(L3, L2)
         # b3(L3, 1)
 
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) / np.sqrt(2/layer_dims[l - 1])
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * np.sqrt(2/layer_dims[l - 1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
 
         assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
